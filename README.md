4d587e0dd2a6f6671a487fef88872eff74c11cc3
davidkimich <116656588+davidkimich@users.noreply.github.com>
2024-08-12 20:18:26 +0300

    uploaded

--- README.md
+++
@@ -1,2 +0,0 @@
-# javatechfinal
-The updated javatech project that uses Api Instead of the built in database manager. The files include a Read me with all the details about the project.
--- src/main/java/com/csis231/api/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csis231.api.repository;
-
-import com.csis231.api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
